c33844a0671e774f673fe3eb96ddc6f3fc322ac6
plamenyovchev <plamenkooo@gmail.com>
2020-01-26 14:43:06 +0200

    LinkedList initial commit

---
+++ data-structures/linked-list/LinkedList.js
@@ -0,0 +1,18 @@
+const Node = require('./Node');
+
+class LinkedList {
+    constructor(value) {
+        this.head = new Node(value);
+        this.tail = this.head;
+        this.length = 1;
+    }
+
+    append(value) {
+        const newNode = new Node(value);
+        this.tail.next = newNode;
+        this.tail = newNode;
+        this.length++;
+    }
+}
+
+module.exports = LinkedList;
