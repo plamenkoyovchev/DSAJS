fddf9e8d5af6e9577400928eeac072c61e685b4d
plamenyovchev <plamenkooo@gmail.com>
2020-01-26 20:47:45 +0200

    insert & remove initial commit

--- data-structures/linked-list/LinkedList.js
+++ data-structures/linked-list/LinkedList.js
@@ -20,6 +20,53 @@ class LinkedList {
         this.length++;
     }
 
+    insert(index, value) {
+        const newNode = new Node(value);
+        if (index <= 0) {
+            this.prepend(newNode);
+            return;
+        }
+
+        if (index >= this.length) {
+            this.append(newNode);
+            return;
+        }
+
+        const leader = this.findLeader(index - 1);
+        const nodeToMoveForward = leader.next;
+        leader.next = newNode;
+        newNode.next = nodeToMoveForward;
+    }
+
+    remove(index) {
+        if (index < 0 || index >= this.length) {
+            return;
+        }
+
+        if (index === 0) {
+            const unwantedNode = this.head;
+            this.head = unwantedNode.next;
+            return;
+        }
+
+        const leader = this.findLeader(index - 1);
+        const unwantedNode = leader.next;
+        leader.next = unwantedNode.next;
+    }
+
+    findLeader(index) {
+        let currentNode = this.head;
+        let counter = 0;
+        while (currentNode) {
+            if (counter === index) {
+                return currentNode;
+            }
+
+            counter++;
+            currentNode = currentNode.next;
+        }
+    }
+
     toArray() {
         const array = [];
         let currentNode = this.head;
