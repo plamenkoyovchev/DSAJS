92d8a28b72c52ff06ba670e988c1b480961c6a0a
plamenyovchev <plamenkooo@gmail.com>
2020-01-28 22:05:02 +0200

    DoublyLinkedList initial commit

---
+++ data-structures/doubly-linked-list/DoublyLinkedList.js
@@ -0,0 +1,99 @@
+const Node = require('./Node');
+
+class DoublyLinkedList {
+    constructor(value) {
+        this.head = new Node(value);
+        this.tail = this.head;
+        this.length = 1;
+    }
+
+    append(value) {
+        const newNode = new Node(value);
+        const oldTail = this.tail;
+
+        this.tail = newNode;
+        newNode.prev = oldTail;
+        oldTail.next = this.tail;
+
+        this.length++;
+    }
+
+    prepend(value) {
+        const newNode = new Node(value);
+        newNode.next = this.head;
+        this.head.prev = newNode;
+        this.head = newNode;
+
+        this.length++;
+    }
+
+    insert(index, value) {
+        if (index < 0 || index >= this.length) {
+            return undefined;
+        }
+
+        if (index === 0) {
+            this.prepend(value);
+        } else if (index === this.length - 1) {
+            this.append(value);
+        } else {
+            const newNode = new Node(value);
+            let leader = this.findLeader(index - 1);
+            let rightNode = leader.next;
+
+            leader.next = newNode;
+            newNode.prev = leader;
+            newNode.next = rightNode;
+            leader = newNode;
+
+            this.length++;
+        }
+    }
+
+    remove(index) {
+
+    }
+
+    indexOf(value) {
+        let counter = 0;
+        let currentNode = this.head;
+        while (currentNode) {
+            if (currentNode.value === value) {
+                return counter;
+            }
+
+            counter++;
+            currentNode = currentNode.next;
+        }
+
+        return undefined;
+    }
+
+    toArray() {
+        const array = [];
+        let currentNode = this.head;
+        while (currentNode) {
+            array.push(currentNode.value);
+            currentNode = currentNode.next;
+        }
+
+        return array;
+    }
+
+    findLeader(index) {
+        let counter = 0;
+        let currentNode = this.head;
+        while (currentNode) {
+            if (counter === index) {
+                return currentNode;
+            }
+
+            counter++;
+            currentNode = currentNode.next;
+        }
+
+        return undefined;
+    }
+}
+
+module.exports = DoublyLinkedList;
---
+++ data-structures/doubly-linked-list/test.js
@@ -0,0 +1,2 @@
+const Node = require('./Node');
+const DoublyLinkedList = require('./DoublyLinkedList');
