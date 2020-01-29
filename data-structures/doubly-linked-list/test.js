fadeb2e74a73089ba9f23a01f286b3fb1dac6fbd
plamenyovchev <plamenkooo@gmail.com>
2020-01-29 23:50:18 +0200

    DoublyLinkedList tests initial commit

--- data-structures/doubly-linked-list/test.js
+++ data-structures/doubly-linked-list/test.js
@@ -1,2 +1,15 @@
 const Node = require('./Node');
 const DoublyLinkedList = require('./DoublyLinkedList');
+
+describe('Node', () => {
+    test('is a class', () => {
+        expect(typeof Node.prototype.constructor).toEqual('function')
+    });
+
+    test('new node has properties data, next and prev that data equals a and next equals null and prev equals null', () => {
+        const node = new Node('a');
+        expect(node.value).toEqual('a');
+        expect(node.next).toEqual(null);
+        expect(node.prev).toEqual(null);
+    });
+});
