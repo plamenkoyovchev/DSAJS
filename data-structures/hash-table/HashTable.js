4fca4b838ac2daa8ad7c198f9c1c7f4c4a3bccc7
plamenyovchev <plamenkooo@gmail.com>
2020-02-20 21:20:15 +0200

    Implement add & get functions

---
+++ data-structures/hash-table/HashTable.js
@@ -0,0 +1,38 @@
+class HashTable {
+    constructor(size) {
+        this.data = new Array(size);
+    }
+
+    _hash(key) {
+        let hash = 0;
+        for (let i = 0; i < key.length; i++) {
+            hash = (hash + key.charCodeAt(i) * i) % this.data.length;
+        }
+
+        return hash;
+    }
+
+    add(key, value) {
+        const address = this._hash(key);
+        if (!this.data[address]) {
+            this.data[address] = [];
+        }
+
+        this.data[address].push([key, value]);
+    }
+
+    // address -> [['grapes', 10000], ['gr', 1]]
+    get(key) {
+        const address = this._hash(key);
+        const dataBucket = this.data[address];
+        if (!dataBucket) {
+            return undefined;
+        }
+
+        for (let i = 0; i < dataBucket.length; i++) {
+            if (dataBucket[i][0] === key) {
+                return dataBucket[i][1];
+            }
+        }
+    }
+}
