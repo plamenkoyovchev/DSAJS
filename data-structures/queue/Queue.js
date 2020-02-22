6602c54779c791016b42bd0eab81929b046e96a6
plamenyovchev <plamenkooo@gmail.com>
2020-02-22 17:41:18 +0200

    Queue implementation with LinkedList initial commit

---
+++ data-structures/queue/Queue.js
@@ -0,0 +1,27 @@
+const Node = require('../linked-list/Node');
+
+class Queue {
+    constructor() {
+        this.top = null;
+        this.bottom = null;
+        this.length = 0;
+    }
+
+    peek() {
+
+    }
+
+    enqueue(value) {
+
+    }
+
+    dequeue() {
+
+    }
+
+    empty() {
+
+    }
+}
+
+module.exports = Queue;
---
+++ data-structures/queue/test.js
@@ -0,0 +1,7 @@
+const Queue = require('./Queue');
+
+describe('Queue', () => {
+    test('Queue enqueue 1 element', () => {
+
+    });
+});
