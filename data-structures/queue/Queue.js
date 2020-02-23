c8d89137f05c047c78a7e9881e6f30f8fb9690a5
plamenyovchev <plamenkooo@gmail.com>
2020-02-23 16:40:02 +0200

    Implement Queue and tests

--- data-structures/queue/Queue.js
+++ data-structures/queue/Queue.js
@@ -8,19 +8,49 @@ class Queue {
     }
 
     peek() {
-
+        return this.last;
     }
 
     enqueue(value) {
+        const newNode = new Node(value);
+        if (this.first === null) {
+            this.first = this.last = newNode;
+        } else {
+            const refToLast = this.last;
+            this.last = newNode;
+            this.last.next = refToLast;
+        }
 
+        this.length++;
     }
 
     dequeue() {
+        if (!this.first) {
+            return null;
+        }
 
+        if (this.first === this.last) {
+            const node = this.first;
+            this.first = this.last = null;
+            this.length--;
+
+            return node;
+        }
+
+        let currentNode = this.last;
+        while (currentNode && currentNode.next !== this.first) {
+            currentNode = currentNode.next;
+        }
+
+        const node = this.first;
+        this.first = currentNode;
+        this.length--;
+
+        return node;
     }
 
     empty() {
-
+        return this.length === 0;
     }
 }
 
--- data-structures/queue/test.js
+++ data-structures/queue/test.js
@@ -1,7 +1,109 @@
 const Queue = require('./Queue');
 
 describe('Queue', () => {
-    test('Queue enqueue 1 element', () => {
+    test('Queue is a class', () => {
+        expect(typeof Queue.prototype.constructor).toEqual('function');
+    });
 
+    test('can add elements to a queue', () => {
+        const q = new Queue();
+        expect(() => {
+            q.enqueue(1);
+        }).not.toThrow();
+    });
+
+    test('can remove elements from a queue', () => {
+        const q = new Queue();
+        expect(() => {
+            q.enqueue(1);
+            q.dequeue();
+        }).not.toThrow();
+    });
+
+    test('enqueue 1 element', () => {
+        const queue = new Queue();
+
+        queue.enqueue(1);
+
+        expect(queue.first.value).toEqual(queue.last.value);
+        expect(queue.length).toEqual(1);
+    });
+
+    test('dequeue 1 element', () => {
+        const queue = new Queue();
+
+        queue.enqueue(1);
+        const dequeuedNode = queue.dequeue();
+
+        expect(dequeuedNode.value).toEqual(1);
+        expect(queue.length).toEqual(0);
+    });
+
+    test('first should equal last if only one element is left', () => {
+        const queue = new Queue();
+
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3);
+
+        queue.dequeue();
+        queue.dequeue();
+
+        const first = queue.first;
+        const last = queue.last;
+
+        expect(first).toEqual(last);
+        expect(first.value).toEqual(3);
+        expect(last.value).toEqual(3);
+    });
+
+    test('Order of elements is maintained', () => {
+        const q = new Queue();
+        q.enqueue(1);
+        q.enqueue(2);
+        q.enqueue(3);
+        expect(q.dequeue().value).toEqual(1);
+        expect(q.dequeue().value).toEqual(2);
+        expect(q.dequeue().value).toEqual(3);
+        expect(q.dequeue()).toEqual(null);
+    });
+
+    test('peek empty queue should return null', () => {
+        const queue = new Queue();
+
+        expect(queue.peek()).toEqual(null);
+    });
+
+    test('peek should return last added element', () => {
+        const queue = new Queue();
+
+        queue.enqueue(1);
+        queue.enqueue(2);
+
+        const peek = queue.peek();
+        expect(peek).not.toEqual(null);
+        expect(peek.value).toEqual(2);
+    });
+
+    test('empty should be true if queue is newly instantiated', () => {
+        const queue = new Queue();
+
+        expect(queue.empty()).toEqual(true);
+    });
+
+    test('empty should be false if element is added to the queue', () => {
+        const queue = new Queue();
+        queue.enqueue(1);
+
+        expect(queue.empty()).toEqual(false);
+    });
+
+    test('empty should be false if element is added to the queue and then removed', () => {
+        const queue = new Queue();
+        queue.enqueue(1);
+        expect(queue.empty()).toEqual(false);
+
+        queue.dequeue();
+        expect(queue.empty()).toEqual(true);
     });
 });
