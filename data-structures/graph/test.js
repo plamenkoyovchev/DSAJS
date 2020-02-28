97255e5dc97cad809cba72e1dcb66561eff79104
plamenyovchev <plamenkooo@gmail.com>
2020-02-28 19:30:52 +0200

    Build graph initial commit

---
+++ data-structures/graph/test.js
@@ -0,0 +1,31 @@
+class Graph {
+
+    constructor() {
+        this.numberOfNodes = 0;
+        this.adjacentList = {};
+    }
+
+
+    addVertex(node) {
+
+    }
+
+    addEdge(node1, node2) {
+
+    }
+
+    showConnections() {
+        const allNodes = Object.keys(this.adjacentList);
+        for (let node of allNodes) {
+            let nodeConnections = this.adjacentList[node];
+            let connections = "";
+            let vertex;
+            for (vertex of nodeConnections) {
+                connections += vertex + " ";
+            }
+            console.log(node + "-->" + connections);
+        }
+    }
+}
+
+module.exports = Graph;
